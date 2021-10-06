b7f3545b8240ce951504e3e63e5fa742b14587ce
x-i-n-jiang <sdlyjx@gmail.com>
2021-10-05 18:46:23 -0700

    update assigment_2.R

---
+++ assignment_2.R
@@ -0,0 +1,58 @@
+# pull first
+
+library(tidyverse)
+library(dplyr)
+
+# 1.setting up the data
+#(a) read data and save as a tibble
+airbnb <- read.csv("assign_2.csv")
+airbnb <- tibble(airbnb)
+
+# (b) view data and column names
+View(airbnb)
+colnames(airbnb)
+
+# (c) rename a column name and save
+airbnb<-
+  airbnb %>%
+  rename(c("neighborhood"="neighbourhood"))
+
+# check colnames(airbnb)
+
+# 2. piping practice
+#(a) count the number of occurrences each neighborhood 
+neighborhoods <-
+  airbnb %>% count(neighborhood, sort=T)
+
+#(b) get rid of any NA rows
+neighborhoods <-
+  neighborhoods %>% filter(!is.na(neighborhood))
+
+# get the top 20 most frequently listed neighborhoods in descending order
+neighborhoods <-
+  neighborhoods %>% arrange(desc(n)) %>% head(20)
+
+#(c) select airbnb data whose neighborhood is in the neighborhoods tibble
+airbnb_top_neighborhoods <-
+  airbnb %>% filter(neighborhood %in% neighborhoods$neighborhood)
+
+#(d) create summary statistics
+summary_stats_top_neighborhoods <-
+  airbnb_top_neighborhoods %>% group_by(neighborhood) %>%
+  summarize(avg_square_feet = mean(square_feet, na.rm = T),
+            avg_price = mean(price, na.rm = T),
+            sd_price = sd(price, na.rm = T),
+            max_price = max(price, na.rm = T),
+            min_price = min(price, na.rm = T))
+
+# avg_square_feet is in descending order
+summary_stats_top_neighborhoods <- 
+  summary_stats_top_neighborhoods %>% arrange(desc(avg_square_feet))
+
+# (e) save the highest average square feet
+highest_avg_square_ft <- summary_stats_top_neighborhoods$avg_square_feet[1]
+
+# (f) save the second highest average price
+value_avg_price <- summary_stats_top_neighborhoods$avg_price
+value_avg_price <- sort(value_avg_price,decreasing=T)
+second_avg_price <- value_avg_price[2]
